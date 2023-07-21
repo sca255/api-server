7b88e283c57a54ebb046b8cdab0b8c16bccc93a3
arpan das <admin@27jb48.onmicrosoft.com>
2023-07-21 14:07:33 +0530

    Add project files.

---
+++ api server/api_server.py
@@ -0,0 +1,12 @@
+from flask import Flask, json, jsonify
+project_db = [{"id" : 1, "name" : "pygame"}, {"id" : 2 , "name" : "webserver"}]
+
+api = Flask(__name__)
+@api.route('/project_db/<int:id>', methods = ["GET", "POST"])
+def returnproject(id): 
+        return jsonify(project_db[id-1])
+
+@api.route()
+
+if __name__ == '__main__':
+    api.run(port = 8888,host = '0.0.0.0')
